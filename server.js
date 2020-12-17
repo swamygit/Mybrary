05b418de21f32b4ffba324ef74e7788426c9546e
swamydhulipalla@gmail.com <swamydhulipalla@gmail.com>
2020-12-17 07:47:41 +0530

    v0

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser : true, useUnifiedTopology: true
     })
 
-const db = connection
+const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 app.use('/', indexRouter)
